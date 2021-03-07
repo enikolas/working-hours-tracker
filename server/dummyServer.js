43b6d4eb2dc8f43d324dbfbef7c13766ebd14eb9
Everton Nikolas de Oliveira <enikolas.oliveira@gmail.com>
2021-03-07 10:11:41 -0300

    Remove old dummy server

--- server/dummyServer.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const mockserver = require('mockserver');
-const logger = require('./logger');
-
-const dummyServerPort = process.env.DUMMY_SERVER_PORT || 9001;
-http.createServer(mockserver(`${__dirname}/dummyServerMocks`)).listen(dummyServerPort);
-logger.info(`Serving dummyServer on ${dummyServerPort}!`);
